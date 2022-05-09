7c411d28bc35fecce2930f62c92875b1df8b6896
DESKTOP-P0LGKND\mate <giorbelidze.mate@kiu.edu.ge>
2022-05-09 14:48:06 +0400

    new img links

--- src/components/ProductService.js
+++ src/components/ProductService.js
@@ -1,21 +1,21 @@
 
 const getProducts = [
     {
-        image: 'https://s3-alpha-sig.figma.com/img/2154/37bd/451ae4ab16fc70ba06d4edea9538f62d?Expires=1652054400&Signature=fX1ywnfM3NmvFleV7pzpj7~rRMZm-IoTT3B28p4IB7h37RZJeTOzjPk3PGQYSrCtNg9YJM2xIyUc~pIOLOJPwU2SyV17GBh1mZ7tU0SdzDJSNeCbjSv2DTVDJz0fY0Z3VaOlieL-BB4EdIRJLjat9nDb6F8vvSuEDOPxDrD93YE5B-Ra3kxbFhSw~z7wG52HFVDsEcnqRnzHz7VuQVWw~H9pi9eXGknIGGw6zxOE2hDXRoshc0Y76Ot-93xwhxpDNtqxJaBLeCdfijp3zJjKLZ70UFrnOHapsNRJJAkCTrKfpqo7i5zcrK13Dwa9m0PdQNwF1fB-CldB831djJ-JuQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+        image: 'https://s3-alpha-sig.figma.com/img/2154/37bd/451ae4ab16fc70ba06d4edea9538f62d?Expires=1653264000&Signature=CDxNiqmxnSzLoyPMTQlxmfzTEDQ-NCe1OzEHnuejeo63kwcHSEviIA9IL0A9DWJtWxCLJflI9TZiiOQNOqNv5HkuHV4jEsCvxNNciOJwfa~o2VnkOyhwnNys3qVWM0Ctq7lHIzbFiVTfHnAXtJ4wGXRCI8grtrzc0mR5ojBL-DUr8jqk9IoqxAhT0ACs67XSWKjaAM2vz55VCjjWa3G9m7AnPyFcxZ2i5trSorKv~0DfsUzikbZa-89pGLS5z0sNpBzoIiF7za8B~efAkDdhrACAbLry4eb7vQwqIt2CIus8AGZhJRFQ1FXv6Mmoqha1wDC3fUBQKqcuMjwAn7mQsA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
         name: 'Monstera',
         price: 39,
         type: 'featured',
         sale: 0
     },
     {
-        image: 'https://s3-alpha-sig.figma.com/img/7f77/9e74/387e1535f72c92f17d2aa7e88ab50281?Expires=1652054400&Signature=QKuY2fPDB1~1I25CXNS5ddGW6sd8fjabBRv1n7R77VzGlhQ05-34NZpeGvKRUeCSIPt-t0vW0x8sLsvIXiEGPPEs1Wttu3B3uWbRvnPZq4SRrzl~h2ghWFH8DTzJwmg7p5w7IoFvuP8y0DpVV03OEiItWBkNH~aB8cy28TyG375emvIebGtJDi6a34jQZ0fqIHit288LTsxpd09sLzPaUhxAQGuOgts573FbPcZBgSLZhV16KV4VDddq1XDJxi4ts-Y-9QqFveXQTx5Ln~JD2cp7Hu~O4djcyzucor8hiyW6v5QKWxLCCt4NXHWyDsv2xr4-aeFRoIqlDW6gi7EWjg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+        image: 'https://s3-alpha-sig.figma.com/img/7f77/9e74/387e1535f72c92f17d2aa7e88ab50281?Expires=1653264000&Signature=e5azh8~W3Q8r9aEkvgUeK2cMrpAoEHKr1vGoUmYlJz-a5cGl2N6SEYThunsPOP8oXnUssoacJIpuzM2wACTOzzEMVxdIpN914qnBrMIXgrcYqMRemvHjBxkcF5fsLeJemcZuhkr1txELrnO~BE0~qJrbCfJbOgHDN9~SvfdmPx3su5eSceDf2uwygNafZjUN8gPAluVYAZdqqCMvgEUaVb5AT85LTQSGSTBV2lL5TQffQFSmzQx6cx6jttbmvF-csJx8nUWZVkqkzaVEipj3dxIM9Be7o~8~~REMqc2xMzQ9Kq~oYsjtSHQmJx~ZSzLyn-wPbSbEn2k-4rvR~Ljnxw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
         name: 'Snake Plant',
         price: 39,
         type: 'featured',
         sale: 0
     },
     {
-        image: 'https://s3-alpha-sig.figma.com/img/aca0/b4c6/60b24f9eddec8e5f42fed17503a4e44d?Expires=1652054400&Signature=g8M9-Kmxn8OkrbnyRg~LAJ21k0O85yEaidnj1aysILNVmOMU24L7zbaa3KczNNXFwOVki2zS7gFeHng5BxJfZfYVoCpPxCsM7kdvSPNqgMpQO5vEE35pbG3chAdsuy-C32tF-tHtT6ymyiBzMeR5dzuRt0RelW6~JfNv7sH2um0KirBPRXFq-sEykMi64TrWagyc8mKEM9lof3tRIeOZiFi9szezsjILcYPAB-zQEuzHWs4SOhXAVlor~F5dMQZtEFiI49FPR2GNh~K65otNV9~sPvebtV2~ghN1Dt1VrpEJ6BSPLDDqnDb-JgUjsdWZ31cCyX2RWO419FjJnzAu0A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+        image: 'https://s3-alpha-sig.figma.com/img/aca0/b4c6/60b24f9eddec8e5f42fed17503a4e44d?Expires=1653264000&Signature=ANQxEPiKmNoifaUcd9nwWTAI~BsagVK09lS-PJUPG9nmDxt9o9Nq93~vsMY1~1hnhtSzHD3jTZPm0~4bZxmKqmEi4U9adytkem6Vzrd5Rn62JyfyAttM2u5yHZ2H6uoq1vakoA9Ns~3sYu6Rs9hwjoV9OSAFeTtirK6dNY57D0uFu85cwmWCTLbjU6GA6keOLWHzESPEUhTaT-F7x301w5ywIZpByaclgoLcm~eBYDGiXSK4-TC-ugDpjCSKiTUnw2VbY8tIxHxQRwcgq-1r~E7P39fquPM3X1yIUzazH1ZNJCslse9iexXFqiodxUDGprOpPA8LGg3rrKZonUgm2w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
         name: 'Palm',
         price: 40,
         type: 'featured',
