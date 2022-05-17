7a72f6f5ee551508df591e766ff3fb6604af1352
kiu_tsertsvadze.nato <tsertsvadze.nato@kiu.edu.ge>
2022-05-17 12:00:52 +0400

    dsfa

---
+++ src/components/Verified.js
@@ -0,0 +1,13 @@
+const Verified = () => {
+    return (
+        <div className="verifyMain">
+           <h1>HeaderSheedh</h1>
+        </div>
+    );
+}
+ 
+export default Verified;
+
+<style>
+
+</style>
