fb5c1293c5adb0248eb74b65d4c2bd904d328301
DESKTOP-P0LGKND\mate <giorbelidze.mate@kiu.edu.ge>
2022-06-10 17:31:48 +0400

    added username

---
+++ src/components/SearchPage.js
@@ -0,0 +1,15 @@
+import './SearchPage.css';
+import Header from './Header';
+
+const SearchPage = () => {
+
+    return (
+        <div className='searchpage'>
+            <Header />
+            <h1>Search page!</h1>
+        </div>
+    )
+
+}
+
+export default SearchPage;
